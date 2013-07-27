d08e0f21b5738aa5a8985772df04bfec4d7e8229
Karthikeyan Vaithilingam <seenukarthi@gmail.com>
2013-07-27 09:14:13 +0400

    JSON Testing

--- springmvc-webapp/src/main/java/com/seenukarthi/springmvc/HelloController.java
+++ springmvc-webapp/src/main/java/com/seenukarthi/springmvc/HelloController.java
@@ -1,9 +1,13 @@
 package com.seenukarthi.springmvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang.SerializationUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,6 +20,15 @@ public class HelloController {
 		TestTO testTO2 = (TestTO) SerializationUtils.clone(testTO1);
 		System.out.println(testTO2.getTestStr());
 		return "hello";
+	}
+	
+	@RequestMapping("/getJson.html")
+	public @ResponseBody List<TestTO> getJson(Model model){
+		List<TestTO> testTOs = new ArrayList<>();
+		TestTO testTO1 = new TestTO();
+		testTO1.setTestStr("Testing");
+		testTOs.add(testTO1);
+		return testTOs;
 	}
 	
 }
